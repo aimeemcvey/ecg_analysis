63ca7e6ba8f8f6d4a6cf0717c0cf235fd109a603
Aimee McVey <ajm111@duke.edu>
2020-02-21 18:21:21 -0500

    Log error when non-numeric string encountered

--- ecg_analysis.py
+++ ecg_analysis.py
@@ -3,13 +3,17 @@
 
 def load_data():
     import csv
-    with open("test_data/test_data1.csv", newline='') as csvfile:
+    f = "test_data/test_data30.csv"
+    with open(f, newline='') as csvfile:
         ecgreader = csv.reader(csvfile, delimiter=' ')
         time, voltage = organize_data(ecgreader)
     return time, voltage
 
 
 def organize_data(filereader):
+    import logging
+    logging.basicConfig(filename="ecg_analysis.log", filemode="w",
+                        level=logging.INFO)
     time = list()
     voltage = list()
     for row in filereader:
@@ -18,7 +22,11 @@ def organize_data(filereader):
         # if either value missing, has non-numeric string, or is Nan
         # log error to log file, skip to next pair: missing_val()
         # w try/except
-        time.append(float(line[0]))
+        try:
+            time.append(float(line[0]))
+        except ValueError:
+            logging.error("Non-numeric string")
+            continue
         # if voltage reading outside +/- 300 mV, add warning to log
         # file w name of test file and voltages exceeding
         # only once per file: bad_val()
