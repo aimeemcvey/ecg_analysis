5cb19bdc476b3a8b815615e9606a5abaeb1a1278
Aimee McVey <ajm111@duke.edu>
2020-02-21 17:07:52 -0500

    Add function framework for analyzing ecg trace

--- ecg_analysis.py
+++ ecg_analysis.py
@@ -5,7 +5,8 @@ def load_data():
     import csv
     with open("test_data/test_data1.csv", newline='') as csvfile:
         ecgreader = csv.reader(csvfile, delimiter=' ')
-        organize_data(ecgreader)
+        time, voltage = organize_data(ecgreader)
+    return time, voltage
 
 
 def organize_data(filereader):
@@ -15,16 +16,24 @@ def organize_data(filereader):
         for r in row:
             line = r.split(',')
         # if either value missing, has non-numeric string, or is Nan
-        # log error to log file, skip to next pair
+        # log error to log file, skip to next pair: missing_val()
+        # w try/except
         time.append(float(line[0]))
         # if voltage reading outside +/- 300 mV, add warning to log
         # file w name of test file and voltages exceeding
-        # only once per file
+        # only once per file: bad_val()
         voltage.append(float(line[1]))
     print(time)
     print(voltage)
     return time, voltage
 
 
+def analyze_trace(time, voltage):
+    import matplotlib.pyplot as plt
+    plt.plot(t, v)
+    plt.show()
+
+
 if __name__ == "__main__":
-    load_data()
+    t, v = load_data()
+    analyze_trace(t, v)
