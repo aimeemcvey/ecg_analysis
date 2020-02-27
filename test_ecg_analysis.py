57d22136048b4e7c142a8d1f09a9049e65553da4
Aimee McVey <ajm111@duke.edu>
2020-02-26 22:40:39 -0500

    Test for multiple timespan options

--- test_ecg_analysis.py
+++ test_ecg_analysis.py
@@ -2,8 +2,19 @@
 import pytest
 
 
-def test_duration():
+# def test_duration():
+#     from ecg_analysis import duration
+#     answer = duration([0.3, 0.8, 1.1, 1.5, 1.8, 2.2, 3.4])
+#     expected = 3.1
+#     assert answer == expected
+
+
+@pytest.mark.parametrize("t, expected", [
+    ([0.3, 0.8, 1.1, 1.5, 1.8, 2.2, 3.4], 3.1),
+    ([0, 0.2, 0.7, 5.6, 9.3, 11.6], 11.6),
+    ([1.1, 1.5, 1.7, 1.9, 2.5, 3.6], 2.5),
+])
+def test_duration(t, expected):
     from ecg_analysis import duration
-    answer = duration([0.3, 0.8, 1.1, 1.5, 1.8, 2.2, 3.4])
-    expected = 3.1
+    answer = duration(t)
     assert answer == expected
