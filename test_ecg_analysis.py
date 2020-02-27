28bbd1477657b907ec077ef93a6d0d6d531ac971
Aimee McVey <ajm111@duke.edu>
2020-02-26 22:44:52 -0500

    Catch up testing for voltage_extremes

--- test_ecg_analysis.py
+++ test_ecg_analysis.py
@@ -18,3 +18,14 @@ def test_duration(t, expected):
     from ecg_analysis import duration
     answer = duration(t)
     assert answer == expected
+
+
+@pytest.mark.parametrize("v, expected", [
+    ([0, 0.1, -0.5, 1.2, 2.4, -3.4, 1.0, 2.1], (-3.4, 2.4)),
+    ([-0.1, 0.8, -5.3, 1.8, 3.5, -3.3, 2.4, -3.4], (-5.3, 3.5)),
+    ([0, 0, 0, 0, 0], (0, 0)),
+])
+def test_voltage_extremes(v, expected):
+    from ecg_analysis import voltage_extremes
+    answer = voltage_extremes(v)
+    assert answer == expected
