fdefe795f537424b6743b468f9864f3535c3c898
Aimee McVey <ajm111@duke.edu>
2020-02-26 22:37:37 -0500

    Catch up on testing for duration

--- test_ecg_analysis.py
+++ test_ecg_analysis.py
@@ -1,2 +1,9 @@
 # test_ecg_analysis.py
 import pytest
+
+
+def test_duration():
+    from ecg_analysis import duration
+    answer = duration([0.3, 0.8, 1.1, 1.5, 1.8, 2.2, 3.4])
+    expected = 3.1
+    assert answer == expected
