3493b908cb33eaf1ed96344c5a122a1c21a713c8
Aimee McVey <ajm111@duke.edu>
2020-02-26 22:47:27 -0500

    Catch up testing for create_dict

--- test_ecg_analysis.py
+++ test_ecg_analysis.py
@@ -2,13 +2,6 @@
 import pytest
 
 
-# def test_duration():
-#     from ecg_analysis import duration
-#     answer = duration([0.3, 0.8, 1.1, 1.5, 1.8, 2.2, 3.4])
-#     expected = 3.1
-#     assert answer == expected
-
-
 @pytest.mark.parametrize("t, expected", [
     ([0.3, 0.8, 1.1, 1.5, 1.8, 2.2, 3.4], 3.1),
     ([0, 0.2, 0.7, 5.6, 9.3, 11.6], 11.6),
@@ -29,3 +22,14 @@ def test_voltage_extremes(v, expected):
     from ecg_analysis import voltage_extremes
     answer = voltage_extremes(v)
     assert answer == expected
+
+
+def test_create_dict():
+    from ecg_analysis import create_dict
+    timespan = 4.5
+    extremes = (-3.5, 1.3)
+    answer = create_dict(timespan, extremes)
+    expected = {"duration": 4.5, "voltage_extremes": (-3.5, 1.3)}
+    assert answer == expected
+
+
